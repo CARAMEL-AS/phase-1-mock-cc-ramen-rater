477322823474cf542cd13111fd3c235ca512c250
Aftab Sidhu <Aftab180@gmail.com>
2022-04-13 11:57:09 -0400

    pre test

--- src/index.js
+++ src/index.js
@@ -1,1 +1,22 @@
-// write your code here
+const ramens = {};
+
+const fetchData = async () => {
+    await fetch('http://localhost:3000/ramens/', {    
+    })
+    .then(res => { return res.json() })
+    .then(data => console.log('Data: ',data))
+    .catch(err => console.log('Error: ',err))
+}
+
+const getAllImages = () => {
+    const images = [];
+    Object.keys(ramens).forEach(ramen => {
+        images.push(ramen.image);
+    })
+    return images;
+}
+
+const getSingleImage = () => {
+
+}
+
